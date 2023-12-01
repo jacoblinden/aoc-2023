a66b2e7d4856bc303ceed94f927cc6471a4faafe
Jacob Linden <jacoblinden@hotmail.com>
2023-12-01 17:47:38 +0100

    Day/01

--- day01/index.test.ts
+++ day01/index.test.ts
@@ -2,10 +2,11 @@ import { describe, expect, test } from 'bun:test'
 import { day} from './index.ts'
 
 describe('Bun Tests Examples', () => {
+
   test('Part 1', () => {
-    expect(day.part1()).toBe(1337)
+    expect(day.part1()).toBe(142)
   })
-  test('Part 2', () => {
-    expect(day.part2()).toBe(42)
+      test('Part 2', () => {
+      expect(day.part2()).toBe(281)
+    })
   })
-})
--- day01/index.ts
+++ day01/index.ts
@@ -1,10 +1,50 @@
 import { file } from 'bun'
 
 const input = {
-  s: await file('input.txt').text(),
+  s: await file('input').text(),
   get parse() {
-    return this.s.split('\n').map((x) => Number(x))
+    return this.s.split('\n');
   },
+
+  get numbers() {
+    const regex = /\d+/g;
+    return this.parse.map((x) => x.match(regex)).map((list) => list?.reduce((a, b) => a + b));
+  },
+
+  get numbersWithLetterConversion() {
+    const Map: { [key: string]: string } = { 
+      one: 'one1one', 
+      two: 'two2two', 
+      three: 'three3three', 
+      four: 'four4four', 
+      five: 'five5five', 
+      six: 'six6six', 
+      seven: 'seven7seven', 
+      eight: 'eight8eight', 
+      nine: 'nine9nine', 
+      zero: 'zero0zero' 
+    };
+    return this.parse
+    .map((x) => x.replace(/(zero|one|two|three|four|five|six|seven|eight|nine)/g, (matched) => Map[matched]))
+    .map((x) => x.match(/\d+/g))
+    .map((list) => list?.reduce((a, b) => a + b))
+  },
+
+  get firstAndLast() :string[] { 
+    return this.numbers.map(extractNums);
+  },
+
+  get firstAndLastLetters() :string[]{ 
+    return this.numbersWithLetterConversion.map(extractNums); ;
+  }
+}
+
+
+const extractNums: any = (number? :string) => {
+  if(number === undefined) return 0;
+  if(number.length === 0) throw new Error('number is empty');
+  if(number.length === 1) return number + number;
+  return number[0] + number[number.length - 1];
 }
 
 export const day ={
@@ -12,16 +52,11 @@ export const day ={
   part2: () => part2(),
 }
 
+const part2: any = () => {
+  return input.firstAndLastLetters.map((x: string) => parseInt(x)).reduce((a, b) => a + b);
+}
+
 const part1: any = () => {
-  return input.parse[0];
+  return input.firstAndLast.map((x: string) => parseInt(x)).reduce((a, b) => a + b);
 }
-
-const part2 : any = () => {
-  return input.parse[1];
-}
-
-process.env.part === 'part1' && console.log(day.part1())
-process.env.part === 'part2' && console.log(day.part2())
-
-
-
+56324
